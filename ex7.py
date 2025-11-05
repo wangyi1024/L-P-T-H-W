4658b41ac7404102d86db7224c6219527d26645c
Chen sheng chun <2806904263@qq.com>
2025-11-05 18:25:07 +0800

    测试1

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print ("Mary hah a little lamb.")
+print ("It's fleece was white as {}.".format('snow'))    # snow 要加引号,不加引号被视为变量
+print ("And everywhere that Mary went.")
+print ("." * 10) # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print (end1 + end2 + end3 + end4 + end5 + end6,end =' ')    # 注意这里的end=' '，表示print结束后不换行，而是加一个空格
+print (end7 + end8 + end9 + end10 + end11 + end12)
