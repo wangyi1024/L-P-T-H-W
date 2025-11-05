be25db7a847fbed2c27ee42991bcb74e25c8d089
Chen sheng chun <2806904263@qq.com>
2025-11-05 17:59:39 +0800

    删除练习文件: ex10.py 到 ex25.py 等

--- ex11.py
+++
@@ -1,8 +0,0 @@
-print ("How old are you?", end=' ')         # end = '' keeps the cursor on the same line
-age = input()
-print ("How tall are you?", end=' ')
-height = input()
-print ("How much do you weigh?", end=' ')
-weight = input()
-
-print (f"So, you're {age} years old, {height} tall and {weight} heavy.")
