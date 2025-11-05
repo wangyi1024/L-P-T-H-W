c3bdeccc5c3ae82d9b3bee1c05ea2e334f18a2cb
时拉比 <wangyi001024@gmail.com>
2025-11-05 17:45:17 +0800

    Add files via upload

---
+++ Learning/ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens")
+
+print("Hens" , 25 + 30 / 6)                     # “/”为除号，先除后加
+print("Roosters", 100 - 25 * 3 % 4)             # “*”为乘，“%”为整除的余数
+
+print ("Now I will count the eggs:")      
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)           
+
+print ("Is it true that 3 + 2 < 5 - 7 ?")        
+
+print (3 + 2 < 5 - 7)                           # “5<-2”,输出False
+
+print ("What is 3 + 2?" , 3 + 2)
+print ("What is 5 - 7?", 5 - 7)
+
+print ("Oh , that's why it's False.")
+
+print ("How about some more.")
+
+print ("It is greater?" , 5 > -2)                 # "5 > -2", 输出True
+print (" Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)           # "5 <= -2", 输出False
