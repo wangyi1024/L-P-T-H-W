24175b801f6661efa18bfb98fb8db91cbc90e13a
时拉比 <wangyi001024@gmail.com>
2025-11-05 17:00:37 +0800

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print ("How old are you?", end=' ')         # end = '' keeps the cursor on the same line
+age = input()
+print ("How tall are you?", end=' ')
+height = input()
+print ("How much do you weigh?", end=' ')
+weight = input()
+
+print (f"So, you're {age} years old, {height} tall and {weight} heavy.")
